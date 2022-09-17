9a7445d7129a5df906aa549141241033b8635a2e
Denis <denyashin@yandex.ru>
2022-09-17 18:48:39 +0300

    Create new string

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите количество строк: ");
+       
+        int elements = int.Parse(Console.ReadLine());
+        string[] Array = new string[elements]; 
+        for (int i = 0; i < Array.Length; i++)
+        {
+            Console.Write("Введите строку №{0}:\r\n    ", i + 1);
+            Array[i] = Console.ReadLine(); 
+        }
+
+        Console.WriteLine("Вы ввели следующие строки:");
+    }
+}
